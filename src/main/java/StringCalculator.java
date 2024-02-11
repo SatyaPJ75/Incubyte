9c7a882119d4ae019323bdf9a7340fd40920466f
Satya Jaiswal <satya.jaiswal@airtel.com>
2024-02-11 19:22:22 +0530

    Incubyte String Calculator

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,17 +1,26 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class StringCalculator {
 
     public List<Integer> add(String [] numbers){
+        List<Integer> ans = new ArrayList<>();
+        //AtomicInteger sum = new AtomicInteger();
         Arrays.stream(numbers).forEach(numberString->{
-            int start =0;
+            int sum =0;
             char delemitor = isDelimitorChanged(numberString);
             if(delemitor!=',' && numberString.charAt(3)=='\n'){
                 numberString = numberString.substring(4);
             }
+            String [] digits = numberString.split(String.valueOf(delemitor));
+            for(int i=0;i<digits.length;i++){
+                sum+=Integer.valueOf(digits[i]);
+            }
+            ans.add(sum);
         });
-        return null;
+        return ans;
     }
 
     private char isDelimitorChanged(String numberString) {
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,12 +1,22 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StringCalculatorTest {
 
+    @InjectMocks
+    StringCalculator stringCalculator;
+
     @Test
-    public String addTest(){
-        return null;
+    public void addTest(){
+        String [] numbers = {"1","2","1,2"};
+        List<Integer> list = List.of(1,2,3);
+        assertEquals(stringCalculator.add(numbers), list);
     }
 }
