a87514e161cb75688276dee669d36f5e76b4b045
Satya Jaiswal <satya.jaiswal@airtel.com>
2024-02-12 08:47:30 +0530

    Incubyte String Calculator Adding Javadoc

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,18 +1,21 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 public class StringCalculator {
 
-
+    /**
+     * Adding numbers by separating delimiter
+     * @param numbers
+     * @return
+     */
     public List<Integer> add(List<String> numbers){
         List<Integer> ans = new ArrayList<>();
         numbers.stream().forEach(numberString->{
             int sum =0;
-            String delemitor = isDelimitorChanged(numberString);
-            numberString = handleDifferentDelemitor(delemitor,numberString);
-            String [] digits = numberString.split(delemitor);
+            String deliminator = isDelimiterChanged(numberString);
+            numberString = handleDifferentDelemitor(deliminator,numberString);
+            String [] digits = numberString.split(deliminator);
             checkNewLineNotInBetween(digits);
             for (String digit : digits) {
                 if (digit.contains(StringCalculatorConstant.NEW_LINE))
@@ -28,13 +31,22 @@ public class StringCalculator {
         return ans;
     }
 
+    /**
+     * Handling \n other than in between 2 numbers
+     * @param digits
+     */
     private void checkNewLineNotInBetween(String[] digits) {
         if(digits[0].startsWith(StringCalculatorConstant.NEW_LINE) || digits[digits.length-1].endsWith(StringCalculatorConstant.NEW_LINE)){
             throw new NumberFormatException();
         }
     }
 
-    private String isDelimitorChanged(String numberString) {
+    /**
+     * Handling differt type of delimiter
+     * @param numberString
+     * @return
+     */
+    private String isDelimiterChanged(String numberString) {
         StringBuilder delemitor = new StringBuilder(StringCalculatorConstant.COMMA);
         if(numberString.startsWith(StringCalculatorConstant.DOUBLE_FORWARD)){
             int i= 3;
@@ -47,12 +59,21 @@ public class StringCalculator {
         return delemitor.toString();
     }
 
+    /**
+     * Handling negative numbers
+     * @param num
+     */
     public void handlenegativeNumbers(int num){
         if(num<0){
             throw new NumberFormatException("negative numbers not allowed "+num);
         }
     }
 
+    /**
+     * Handling number greater than bigger number
+     * @param num
+     * @return
+     */
     public int handleBiggerNumbers(int num){
         if(num > StringCalculatorConstant.BIGGER_NUMBER){
             return 0;
@@ -60,6 +81,12 @@ public class StringCalculator {
         return num;
     }
 
+    /**
+     * Handling multiple numbers of different delimiter
+     * @param delemitor
+     * @param numberString
+     * @return
+     */
     public String handleDifferentDelemitor(String delemitor,String numberString) {
         if (!Objects.equals(delemitor, StringCalculatorConstant.COMMA) && numberString.charAt(4 + delemitor.length()) == '\n') {
             numberString = numberString.substring(5 + delemitor.length());
